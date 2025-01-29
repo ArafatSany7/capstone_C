057baadc423d3d42db377d0b9280b0b81f955bd2
Arafat Sany <human.sany7@gmail.com>
2025-01-29 16:05:56 +0600

    testSolve

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    int m, n ;
+
+    printf("Enter two intiger m & n: ");
+    scanf("%d %d",&m ,&n);
+
+
+    if( m == n ){
+        printf("M & N are equal");
+
+    }
+    else{
+        printf("they are not equal");
+    }
+}
