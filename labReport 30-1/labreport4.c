fa8af4311c0e7c440d5f51509b92d87d98672a07
Arafat Sany <human.sany7@gmail.com>
2025-01-31 17:04:12 +0600

    labreport4

--- labReport 30-1/labreport4.c
+++ labReport 30-1/labreport4.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
 int main()
 {
+    int temp;
+
+    printf("Enter your current tempareture: ");
+    scanf("%d", &temp);
+
+    if (temp >= 40)
+    {
+        printf("Its very hot");
+    }
+    else if (temp >= 30 && temp < 40)
+    {
+        printf("It is hot");
+    }
+    else if (temp >= 20 && temp < 30)
+    {
+        printf("Normal in temp");
+    }
+    else if (temp >= 10 && temp < 20)
+    {
+        printf("Its cold");
+    }
+    else if (temp >= 0 && temp < 10)
+    {
+        printf("very cold weather");
+    }
+    else
+    {
+        printf("Freezing weather");
+    }
 }
